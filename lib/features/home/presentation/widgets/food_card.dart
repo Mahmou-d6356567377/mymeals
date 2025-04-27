dbf51cab0ecb103df666af56393a4e2b0ba390e7
Mahmou-d6356567377 <ma2327424@gmial.com>
2025-04-27 16:14:03 +0300

    test1

--- lib/features/home/presentation/widgets/food_card.dart
+++ lib/features/home/presentation/widgets/food_card.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodCard extends StatelessWidget {
@@ -38,17 +39,19 @@ class FoodCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image.network(
-                imagePath,
-                height: 100,
-                width: double.infinity,
-                fit: BoxFit.cover,
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  imagePath,
+                  height: 100,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             const SizedBox(height: 8),
-            Text(
+            AutoSizeText(
               title,
               style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
